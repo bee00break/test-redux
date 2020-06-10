686cc843df5e8cfc5f6987bd559d8ea4195cfc4d
bee00break <ohara199210@gmail.com>
2020-06-10 17:40:21 +0900

    defaultPropsとpropTypes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
 import React from 'react';
-;
-function App() {
-  return <div><h1>Hello, World!</h1></div>;
+import PropTypes from 'prop-types'
+
+// function App() {
+//   return <div><h1>Hello, World!</h1></div>;
+// }
+
+
+// class App extends React.Component {
+//   render(){
+//     return(
+//       <div><h1>Hello, World!</h1></div>
+//     )
+//   }
+// }
+
+const App = () =>{
+  const profiles = [
+    {name: "Taro", age: 10},
+    {name: "Hanako", age: 20},
+    {name: "Hanako"}
+  ]
+  return(
+    <div>
+      {
+        profiles.map((profile, index) =>{
+          return <User name={profile.name} age={profile.age} key={`profile_${index}`}/>
+        })
+      }
+    </div>
+  )
+}
+
+const User = (props) =>{
+  return <div>Hi, I am {props.name}! and {props.age} years old.</div>
+}
+
+//デフォルト
+User.defaultProps = {
+  age: 1
+}
+
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired,
 }
 
 export default App;
